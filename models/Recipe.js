aaef3fc62589796d72da10f9129c81cd5c842ff5
yxxyn20 <dbtndus0527@gmail.com>
2023-01-24 03:48:12 +0900

    Recipe 스키마에 timestamp 추가

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,31 +1,34 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const recipeSchema = mongoose.Schema({
-  beerId: {
-    type: Schema.Types.ObjectId,
-    ref: "Beer",
-  },
-  usserId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  ingredient: [
-    {
+const recipeSchema = mongoose.Schema(
+  {
+    beerId: {
       type: Schema.Types.ObjectId,
-      ref: "Ingredient",
+      ref: "Beer",
     },
-  ],
-  content: {
-    type: String,
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    ingredient: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Ingredient",
+      },
+    ],
+    content: {
+      type: String,
+    },
+    avgScore: {
+      type: Number,
+    },
+    likes: {
+      type: Number,
+    },
   },
-  avgScore: {
-    type: Number,
-  },
-  likes: {
-    type: Number,
-  },
-});
+  { timestamps: true }
+);
 
 const Recipe = mongoose.model("Recipe", recipeSchema);
 
